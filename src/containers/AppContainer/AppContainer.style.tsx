0669b750983f1b9489ed6e4a593d2633a24d9ccf
Marcospantunes <marcospantunes203@gmail.com>
2024-02-28 14:32:20 -0300

    fix: corrigido problema de adaptabilidade do tamanho do container

--- src/containers/AppContainer/AppContainer.style.tsx
+++ src/containers/AppContainer/AppContainer.style.tsx
@@ -1,12 +1,10 @@
 import styled from "styled-components"
 
 const AppContainerStyled = styled.div`
-    width: 100dvw;
-    height: 100dvh;
     display: flex;
     flex-direction: column;
     gap: 30px;
-    padding: 10px;
+    padding: 5px;
     text-align: center;
     color: ${({theme}) => theme.text};
     background-color: ${({theme}) => theme.body};
