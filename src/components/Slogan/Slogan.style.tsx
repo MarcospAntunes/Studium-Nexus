265e3339625703dad3874f10c2c95b0c44492a9b
Marcospantunes <marcospantunes203@gmail.com>
2024-03-20 19:06:15 -0300

    fix: corrigido estilo do slogan

--- src/components/Slogan/Slogan.style.tsx
+++ src/components/Slogan/Slogan.style.tsx
@@ -3,8 +3,12 @@
 import styled from "styled-components";
 
 const SloganStyled = styled.h2`
-    max-width: 760px;
+    font-size: 18px;
     text-align: left;
+
+    @media screen and (min-width: 541px) {
+        font-size: 22px;
+    }
 `
 
 export default SloganStyled;
