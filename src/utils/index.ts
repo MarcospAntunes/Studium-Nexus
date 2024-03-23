50ec507b67c09c6867e3b1e4e55f61ebe1a77182
Marcospantunes <marcospantunes203@gmail.com>
2024-03-23 20:51:10 -0300

    fix: fixed name

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import convertUnitsAndCoins from "./conversions/unitsAndCoins";
 import bhaskara from "./formulas/bhaskara";
 import formulasPages from "./formulas/formulasPages";
 import arithmeticAverage from "./formulas/arithmeticAverage";
-import verifyRoutes from "./verifyRoutes/VerifyRoutes";
+import verifyRoutes from "./verifyRoutes";
 import simpleInterest from "./formulas/simpleInterest";
 
 export {
