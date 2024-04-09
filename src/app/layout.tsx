8639d349ff020e99afe93ad378146f802956693e
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2024-04-09 12:12:09 -0300

    fix: corrigido problema de SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
 import RootLayout from '@studium-nexus/home-app/src/app/layout';
+import { metadata } from "@studium-nexus/home-app/src/app/layout";
 
+export { metadata }
 export default RootLayout
