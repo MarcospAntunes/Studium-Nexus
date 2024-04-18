56dcba2c0525b077ae05fb6cd059b947397c035d
Marcospantunes <marcospantunes203@gmail.com>
2024-04-18 20:07:10 -0300

    docs: configurado o eslinter para funcionar com o react e o typescript

--- config/eslint-commons/base.js
+++ config/eslint-commons/base.js
@@ -1,12 +1,24 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  "extends": [
-    "eslint:recommended",
-    "next",
-    "next/core-web-vitals",
+  "extends": ["eslint:recommended", "next", "next/core-web-vitals"],
+  "globals": {
+    "React": "true",
+    "google": "true",
+    "mount": "true",
+    "mountWithRouter": "true",
+    "shallow": "true",
+    "shallowWithRouter": "true",
+    "context": "true",
+    "expect": "true",
+    "jsdom": "true",
+    "JSX": "true",
+  },
+  "plugins": [
+    "@typescript-eslint"
   ],
   "rules": {
     "no-console": "error",
     "@next/next/no-html-link-for-pages": "off"
   }
-  }
+}
+
