b00033b1f51cfbf218fa40776af1178c563d57b2
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2024-04-24 13:37:22 -0300

    fix: corrigido caminho de importação

--- commons/tests/toBeInTheDocument/toBeInTheDocument.type.ts
+++ commons/tests/toBeInTheDocument/toBeInTheDocument.type.ts
@@ -1,7 +1,7 @@
 import { RenderResult } from "@testing-library/react"
 
 type toBeInTheDocumentProps = {
-  Element: RenderResult<typeof import("c:/Users/silvmar13/Documents/GitHub/Studium-Nexus/node_modules/@testing-library/dom/types/queries"), HTMLElement, HTMLElement>
+  Element: RenderResult<typeof import("@testing-library/dom/types/queries"), HTMLElement, HTMLElement>
   fn: 'getByText' | 'container'
   text?: string
   tag?: string
