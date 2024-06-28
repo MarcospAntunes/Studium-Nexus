b4dedde3b9b397ffc3b31ad5952a936b71933260
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2024-06-28 12:58:33 -0300

    fix: corrigido bug de o tema não mudar

--- src/app/sobre/layout.tsx
+++ src/app/sobre/layout.tsx
@@ -1,14 +1,9 @@
 import StyledComponentsRegistry from "@studium-nexus/lib-commons/registry";
-import Providers from "./providers";
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <StyledComponentsRegistry>
-      <Providers>{children}</Providers>
-    </StyledComponentsRegistry>
-  );
+  return <StyledComponentsRegistry>{children}</StyledComponentsRegistry>;
 }
