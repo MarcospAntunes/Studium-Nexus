737fc4402fdd6f4aad69c9d0bf24e879b953953c
Marcospantunes <marcospantunes203@gmail.com>
2024-07-11 18:04:36 -0300

    Update page.tsx

--- apps/home-app/src/app/page.tsx
+++ apps/home-app/src/app/page.tsx
@@ -11,13 +11,6 @@ import { ButtonContainer, MainContainer } from "../containers";
 export default async function Home() {
   return (
     <AppContainer>
-      <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4555608257448575"
-          crossOrigin="anonymous"
-        ></script>
-      </head>
       <Header />
       <MainContainer>
         <section>
