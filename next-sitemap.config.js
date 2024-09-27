ba4765647a19e77fff759cc2160c65edec39daa0
Marcospantunes <marcospantunes203@gmail.com>
2024-09-27 16:34:43 -0300

    fix: fixed url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /**@type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://studium-nexus.vercel.app/",
+  siteUrl: "https://www.studium-nexus.com.br/",
   generateRobotsTxt: true,
 };
